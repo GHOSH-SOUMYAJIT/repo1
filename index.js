4511702d8e29f79a966555119bb37581c6557d56
GHOSH-SOUMYAJIT <162975986+GHOSH-SOUMYAJIT@users.noreply.github.com>
2024-04-01 15:20:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello Students...')
+  res.send('Hello Soumyajit...')
 })
 
 app.listen(4000, ()=>{
